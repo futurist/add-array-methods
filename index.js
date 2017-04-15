06b6ffb15fe9bdc9ea28fdc464d5daf4cf2e7724
futurist <jamesyang999@gmail.com>
2017-04-15 15:20:23 +0800

    add methodsCache

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+var methodsCache = {}
+
 function addArrayMethods (customMethods, options) {
   options = options || {}
   var method
   var allMethods = {}
-  var nativeMethods = options.natives || [
-    'filter', 'slice', 'concat',
-    'reverse', 'sort', 'splice',
-    'map', 'fill', 'copyWithin'
-  ]
+  var nativeMethods = options.natives || addArrayMethods.natives
   for (var i = 0; i < nativeMethods.length; i++) {
     method = nativeMethods[i]
     if (!(method in Array.prototype)) continue
-    allMethods[method] = new Function(
+    allMethods[method] = methodsCache[method] || (methodsCache[method] = new Function(
       'return this._addArrayMethods(Array.prototype.' + method + '.apply(this, arguments))'
-    )
+    ))
   }
   for (method in customMethods) {
     allMethods[method] = customMethods[method]
@@ -31,5 +29,9 @@ function addArrayMethods (customMethods, options) {
     return arr
   }
 }
-
+addArrayMethods.natives = [
+  'filter', 'slice', 'concat',
+  'reverse', 'sort', 'splice',
+  'map', 'fill', 'copyWithin'
+]
 module.exports = addArrayMethods
