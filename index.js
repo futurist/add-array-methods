5ec57bead2a7b8d04b8d04275e86fc0ef8cd07ac
futurist <jamesyang999@gmail.com>
2017-04-15 16:17:45 +0800

    using wrapperFactory instead of new Function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var methodsCache = {}
-
+var wrapperFactory = function(method){
+  return function(){
+    return this._addArrayMethods(Array.prototype[method].apply(this, arguments))
+  }
+}
 function addArrayMethods (customMethods, options) {
   options = options || {}
   var method
@@ -8,9 +11,7 @@ function addArrayMethods (customMethods, options) {
   for (var i = 0; i < nativeMethods.length; i++) {
     method = nativeMethods[i]
     if (!(method in Array.prototype)) continue
-    allMethods[method] = methodsCache[method] || (methodsCache[method] = new Function(
-      'return this._addArrayMethods(Array.prototype.' + method + '.apply(this, arguments))'
-    ))
+    allMethods[method] = wrapperFactory(method)
   }
   for (method in customMethods) {
     allMethods[method] = customMethods[method]
