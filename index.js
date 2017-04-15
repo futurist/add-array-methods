ff4439020f9e97bacc222a096cc84ec7595cea51
futurist <jamesyang999@gmail.com>
2017-04-15 16:22:30 +0800

    1.1.1 using cache for native methods

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-var wrapperFactory = function(method){
-  return function(){
-    return this._addArrayMethods(Array.prototype[method].apply(this, arguments))
-  }
-}
+var methodsCache = {}
+
 function addArrayMethods (customMethods, options) {
   options = options || {}
   var method
@@ -11,7 +8,9 @@ function addArrayMethods (customMethods, options) {
   for (var i = 0; i < nativeMethods.length; i++) {
     method = nativeMethods[i]
     if (!(method in Array.prototype)) continue
-    allMethods[method] = wrapperFactory(method)
+    allMethods[method] = methodsCache[method] || (methodsCache[method] = new Function(
+      'return this._addArrayMethods(Array.prototype.' + method + '.apply(this, arguments))'
+    ))
   }
   for (method in customMethods) {
     allMethods[method] = customMethods[method]
