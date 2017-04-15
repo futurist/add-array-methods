7bede286638ee5dc2141f137fe91d3ee72086e9d
futurist <jamesyang999@gmail.com>
2017-04-15 10:59:16 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # add-array-methods
-Add custom array methods that can chainable
+Add custom array methods that can chainable, like subclass Array, but not modify Array.prototype
+
+## Install
+
+- NPM
+
+``` bash
+npm i -S add-array-methods
+```
+
+- UNPKG
+
+``` html
+https://unpkg.com/add-array-methods
+```
+
+## Usage
+
+``` javascript
+var myArr = []
+addArraymethods(myArr, {
+  last: function() {
+    return this[this.length-1]
+  },
+  // ... more here
+})
+```
+
+The result is, the `last` methods will always available, even after all **native methods** that returns array, like below:
+
+``` javascript
+myArr.filter(v=>v > 2).sort().last()
+```
+
---
+++ index.js
@@ -0,0 +1,28 @@
+function addArrayMethods (customMethods) {
+  var method
+  var allMethods = {}
+  var nativeMethods = [
+    'filter', 'slice', 'concat',
+    'reverse', 'sort', 'splice',
+    'map', 'fill', 'copyWithin'
+  ]
+  for (var i = 0; i < nativeMethods.length; i++) {
+    method = nativeMethods[i]
+    if (!(method in Array.prototype)) continue
+    allMethods[method] = new Function(
+      'return this._addArrayMethods(Array.prototype.' + method + '.apply(this, arguments))'
+    )
+  }
+  for (method in customMethods) {
+    allMethods[method] = customMethods[method]
+  }
+  return function (arr) {
+    arr._addArrayMethods = this._addArrayMethods
+    for (method in allMethods) {
+      arr[method] = allMethods[method]
+    }
+    return arr
+  }
+}
+
+module.exports = addArrayMethods
